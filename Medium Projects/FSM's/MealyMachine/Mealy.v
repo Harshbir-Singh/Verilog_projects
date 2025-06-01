ea53aea03039609ca2b880dfed2b46b0772b939e
Harshbir-Singh <harshbir1279@gmail.com>
2025-06-01 14:48:22 +0530

    Update Mealy.v

--- Medium Projects/FSM's/MealyMachine/Mealy.v
+++ Medium Projects/FSM's/MealyMachine/Mealy.v
@@ -1,26 +1,68 @@
-module mealy(Y,x,clk);
-  input x,clk;
-  output Y;
-
-  wire in1,in2,A,B,C;
-
-  FlipFlop D1(A,in1,clk);
-  FlipFlop D2(B,in2,clk);
+//1 process methodology
+module mealy(clk,rst,din,dout);
+  input clk,rst,din;
+  output reg dout;
   
-  assign Y = C|x;
-  assign C = A&B;
-  assign in1 = x&B;
-  assign in2 = ~x|A;
+  parameter idle = 0;
+  parameter s0 = 1;
+  parameter s1 = 2;
+  parameter s2 = 3;
 
-
-endmodule
-
-module FlipFlop(out,in,clk);
-  output reg out;
-  input in,clk;
-
+  
+  reg [2:0] state = idle;
+  
   always@(posedge clk)
-    begin
-      out<=in;
+    begin 
+      if(rst == 1'b1)
+        begin
+          state <= idle;
+        end
+      else
+        case(state) 
+          idle: 
+            begin
+              state=s0;
+              dout=1'b0;
+            end
+          s0:
+            begin
+              if(din==1'b0)
+                begin
+                  state = s2;
+                  dout = 1'b0;
+                end
+              else
+                begin
+                  state = s1;
+                  dout = 1'b0;
+                end
+            end
+          s1:
+            begin
+              if(din==1'b0)
+                begin
+                  state = s2;
+                  dout = 1'b0;
+                end
+              else
+                begin
+                  state = s1;
+                  dout = 1'b1;
+                end
+            end
+          s2:
+            begin
+              if(din==1'b0)
+                begin
+                  state = s2;
+                  dout = 1'b0;
+                end
+              else
+                begin
+                  state = s1;
+                  dout = 1'b0;
+                end
+            end
+        endcase
     end
 endmodule
