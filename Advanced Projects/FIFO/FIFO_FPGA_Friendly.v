fc7f4984feedb0046f00f8d89919bc5e68344263
Harshbir-Singh <harshbir1279@gmail.com>
2026-01-20 21:59:37 +0530

    Update FIFO_FPGA_Friendly.v

--- Advanced Projects/FIFO/FIFO_FPGA_Friendly.v
+++ Advanced Projects/FIFO/FIFO_FPGA_Friendly.v
@@ -29,6 +29,9 @@ module FIFO(
   );
   reg [1:0] wr_sync, rd_sync;
   reg       wr_sync_d, rd_sync_d;
+  reg [20:0] debounce_wr, debounce_rd;
+  reg wr_stable, rd_stable;
+  reg wr_stable_d, rd_stable_d;
 
   always @(posedge clk) begin
     wr_sync <= {wr_sync[0], wr_en};
@@ -37,9 +40,56 @@ module FIFO(
     wr_sync_d <= wr_sync[1];
     rd_sync_d <= rd_sync[1];
   end
-
-  wire wr_pulse = wr_sync[1] & ~wr_sync_d;
-  wire rd_pulse = rd_sync[1] & ~rd_sync_d;
+  always@(posedge clk)
+    begin
+      if(rst)
+        begin
+          debounce_wr <=0;
+          debounce_rd <=0;
+          rd_stable<=0;
+          wr_stable<=0;
+        end
+      else
+        begin
+          if(wr_sync_d)
+            begin
+            if(debounce_wr[20]==0)
+              begin
+                debounce_wr<=debounce_wr+1;
+              end
+            else wr_stable<=1;                        
+            end   
+          else
+            begin
+              debounce_wr<=0; 
+              wr_stable<=0;
+            end
+          if(rd_sync_d)
+            begin
+            if(debounce_rd[20]==0)
+              begin
+                debounce_rd<=debounce_rd+1;  
+              end
+            else
+              begin
+                rd_stable<=1;
+                 
+              end          
+            end
+          else
+            begin
+              debounce_rd<=0; 
+              rd_stable<=0;
+            end 
+        end
+    end
+  always@(posedge clk)
+    begin
+      wr_stable_d<=wr_stable;
+      rd_stable_d<=rd_stable;
+    end
+  wire wr_pulse = wr_stable & ~wr_stable_d;
+  wire rd_pulse = rd_stable & ~rd_stable_d;
 
   reg [3:0] mem [0:511];
   reg [9:0] rd_ptr, wr_ptr;
@@ -58,20 +108,15 @@ module FIFO(
       empty   <= 1'b1;
       rd_data <= 4'b0;
     end else begin
-
-
       if (wr_pulse && !full) begin
         mem[wr_addr] <= wr_data;
         wr_ptr <= wr_ptr + 1'b1;
       end
-
-  
       if (rd_pulse && !empty) begin
         rd_data <= mem[rd_addr];
         rd_ptr <= rd_ptr + 1'b1;
       end
-
-
+      
       empty <= (wr_ptr_next == rd_ptr_next);
       full  <= (wr_ptr_next[8:0] == rd_ptr_next[8:0] &&
                 wr_ptr_next[9]   != rd_ptr_next[9]);
